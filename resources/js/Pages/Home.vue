20be82215aed412e99310ed14d4693141d38f3b7
Yehuda Neufeld <newgraphil@gmail.com>
2020-06-25 14:44:02 +0300

    trigger deploy

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="px-6 py-12 text-teal-800 bg-teal-300 rounded">
-    <h1 class="text-3xl font-black text-center">{{ $page.message }}</h1>
+  <div class="px-6 py-12 text-center text-teal-800 bg-teal-300 rounded">
+    <h1 class="text-3xl font-black">{{ $page.message }}</h1>
+    <p class="mt-4 text-teal-50">Watch this space!</p>
   </div>
 </template>
 
