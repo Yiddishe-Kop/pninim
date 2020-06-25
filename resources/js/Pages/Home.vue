0a5a55b12e8f853f8e03e3745aff57b63807051e
Yehuda Neufeld <newgraphil@gmail.com>
2020-06-25 15:04:36 +0300

    testing server build

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-6 py-12 text-center text-teal-800 bg-teal-300 rounded">
     <h1 class="text-3xl font-black">{{ $page.message }}</h1>
-    <p class="mt-4 text-teal-50">Watch this space!</p>
+    <p class="mt-4 text-teal-50">Watch this space! 💡</p>
   </div>
 </template>
 
